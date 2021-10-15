ae69d4f93ea71d83b5302d8e5530e3a685417315
Matthew Makepeace <Matthewmakepeace32@gmail.com>
2021-10-15 13:47:19 -0400

    Added youtube link to readme file

--- OrgreTemplateV2/Ball.cpp
+++ OrgreTemplateV2/Ball.cpp
@@ -66,28 +66,6 @@ void Ball::Reset()
 	velocity.y *= -1.0f;
 	position = Ogre::Vector2(0, 50);
 
-	int m_random = rand() % 4;
-	float randDir = 0.0f;
-
-	switch (m_random)
-	{
-	case 0:
-		randDir = 45.0f;
-		break;
-	case 1:
-		randDir = 135.0f;
-		break;
-	case 2:
-		randDir = 225.0f;
-		break;
-	case 3:
-		randDir = 315.0f;
-		break;
-	}
-
-	float dirX = 160.0f * cos(randDir * 3.14159f / 180.0f);
-	float dirY = 160.0f * sin(randDir * 3.14159f / 180.0f);
-
 	this->SetPosition(Ogre::Vector3::ZERO);
-	this->SetVelocity(Ogre::Vector2(dirX, dirY));
+	this->SetVelocity(Ogre::Vector2(velocity.x, velocity.y));
 }
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 To go to the doxygen documentation go to the following path:
 docs > html > index.html
+
+
+Youtube Video Demo: https://www.youtube.com/watch?v=GqUiPdstdX4
